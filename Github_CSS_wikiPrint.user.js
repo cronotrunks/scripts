4b38330458497d6fa6bdbb1ca5012d305d1ca21e
Javier Carranza <javier.carranza@crononauta.com>
2012-10-06 13:31:13 +0200

    CSS path reference modified

--- Github_CSS_wikiPrint.user.js
+++ Github_CSS_wikiPrint.user.js
@@ -9,7 +9,7 @@
 
 var link = document.createElement('LINK');
 link.rel = 'stylesheet';
-link.href = 'https://github.com/eurodev/scripts/raw/master/github.css';
+link.href = 'https://raw.github.com/eurodev/scripts/master/github.css';
 link.type = 'text/css';
 link.media = 'print';
 document.body.insertBefore(link, null);

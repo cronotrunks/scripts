3d00f0f74a9dec1e58e97cdbb2365e6f836d78c6
Javier Carranza <javier.carranza@crononauta.com>
2012-10-06 13:28:59 +0200

    CSS path reference modified

--- Github_CSS_wikiPrint.user.js
+++ Github_CSS_wikiPrint.user.js
@@ -9,7 +9,7 @@
 
 var link = document.createElement('LINK');
 link.rel = 'stylesheet';
-link.href = 'http://al.quimia.net/~trunks/github.css';
+link.href = 'https://github.com/eurodev/scripts/raw/master/github.css';
 link.type = 'text/css';
 link.media = 'print';
 document.body.insertBefore(link, null);

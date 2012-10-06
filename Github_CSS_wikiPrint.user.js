fd3875a02b1b792b0915ec9df4f6adc87d6142e7
Javier Carranza <javier.carranza@crononauta.com>
2012-10-06 13:26:07 +0200

    Greasemonkey script to get clean wiki pages on github.como to print documents

---
+++ Github_CSS_wikiPrint.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Github CSS wikiPrint
+// @namespace   githubwikiprint
+// @include     https://github.com/*/wiki
+// @include     https://github.com/*/wiki/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var link = document.createElement('LINK');
+link.rel = 'stylesheet';
+link.href = 'http://al.quimia.net/~trunks/github.css';
+link.type = 'text/css';
+link.media = 'print';
+document.body.insertBefore(link, null);

542bca69502e02dc063432cf5e7e02a4f10265c5
Javier Carranza <javier.carranza@crononauta.com>
2015-06-25 14:47:15 +0200

    Adds usability for desktop users

--- screenshot.sh
+++ screenshot.sh
@@ -31,12 +31,24 @@ function check() {
       echo -e "scrot is not installed or in your path. Please, consider to install it in order to use this script"
       echo
     fi
+
+    DROPBOX_STATUS=$(dropbox status)
+    if [ "${DROPBOX_STATUS}" == "Dropbox isn't running!" ]
+    then
+        ${ZENITY} --question --text="Dropbox must be running\n\nDo you want to launch it?"
+        [ "$?" == "0" ] && dropbox start
+    fi
 }
 
 function do_screenshot() {
 
+    ${ZENITY} --info --text="Select the area to capture.\n\nYou need to close this dialog before it."
+
     ${SCROT} -s -e 'mv $f /tmp/'${SCREENSHOT_FILE}
     mv /tmp/${SCREENSHOT_FILE} ${DROPBOX_HOME}/Public/
+
+    ${ZENITY} --info --text="Wait for publishing the screenshot to dropbox" &
+    ZENITYPID=$!
 
     while ([ "$(${DROPBOX} filestatus ${DROPBOX_HOME}/Public/${SCREENSHOT_FILE} | awk '{ print $2; }')" == "syncing" ])
         do sleep 1
@@ -44,6 +56,7 @@ function do_screenshot() {
 
     PUBLIC_URL=$(${DROPBOX} puburl ${DROPBOX_HOME}/Public/${SCREENSHOT_FILE})
 
+    kill -9 ${ZENITYPID}
     ${ZENITY} --info --text="Dropbox screenshot url: ${PUBLIC_URL}"
 }
 
